058ce90114b777234d1458d6bb790c322ac4210c
ghostfighter50 <ghostfighter5015@gmail.com>
2020-12-06 22:24:42 +0100

    rate limiter enabled

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var express = require('express');
 var fs = require("fs")
 var app = express();
 var bodyParser = require('body-parser');
+const rateLimit = require("express-rate-limit");
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100
+});
+app.use("/users", apiLimiter);
+ 
+const createAccountLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000	,
+  max: 5, 
+  message:
+    "Too many accounts created from this IP, please try again after an hour"
+});
 const readJson = fs.readFileSync('./data/users.json');
 var  data = JSON.parse(readJson);
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,6 +35,13 @@ app.get('/contact', function(req, res) {
 });
 app.get('/add', (req, res) => {
 	res.render('add');
+});
+app.post('/add', createAccountLimiter , (req, res) => {
+	const { title, classe } = req.body;
+
+	data.push({ ID: data.length + 1, Title: title, Classe: classe });
+	fs.writeFileSync('./data/users.json', JSON.stringify(data, null, 4));
+	res.redirect('/users');
 });
 app.get('/users', (req, res) => {
 	const { filter } = req.query;
@@ -44,13 +65,7 @@ app.get('/users', (req, res) => {
 
 	res.render('users', { data: filterData, filter });
 });
-app.post('/add', (req, res) => {
-	const { title, classe } = req.body;
 
-	data.push({ ID: data.length + 1, Title: title, Classe: classe });
-	fs.writeFileSync('./data/users.json', JSON.stringify(data, null, 4));
-	res.redirect('/users');
-});
 app.get('/edit/:id', (req, res) => {
 	const { id } = req.params;
 	let dataId;
